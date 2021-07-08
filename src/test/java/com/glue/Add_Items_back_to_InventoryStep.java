d26d9b75f36334f6e0eeda3c1ff527a8add93e2a
jarrod-grayson <52757778+jarrod-grayson@users.noreply.github.com>
2021-07-08 11:02:59 -0400

    Update Add_Items_back_to_InventoryStep.java

--- src/test/java/com/glue/Add_Items_back_to_InventoryStep.java
+++ src/test/java/com/glue/Add_Items_back_to_InventoryStep.java
@@ -26,4 +26,34 @@ public class Add_Items_back_to_InventoryStep {
     public void I_should_have_four_black_sweaters_in_inventory() throws Throwable {
         Assert.assertEquals(true, true);
     }
+    
+    @Given("^that a customer previously bought a black garment from me$")
+    public void that_a_customer_previously_bought_a_black_garment_from_me() throws Throwable {
+        Assert.assertEquals(true, true);
+    }
+
+    @When("^I have two blue garments in inventory$")
+    public void I_have_two_blue_garments_in_inventory() throws Throwable {
+        Assert.assertEquals(true, true);
+    }
+
+    @When("^three black garments in inventory$")
+    public void three_black_garments_in_inventory() throws Throwable {
+        Assert.assertEquals(true, true);
+    }
+
+    @When("^they exchange the blue garment for a black garment$")
+    public void they_exchange_the_blue_garment_for_a_black_garment() throws Throwable {
+        Assert.assertEquals(true, true);
+    }
+    
+    @Then("^I should have three blue garments in inventory$")
+    public void I_should_have_three_blue_garments_in_inventory() throws Throwable {
+        Assert.assertEquals(true, true);
+    }
+    
+    @When("^two black garments in inventory$")
+    public void two_black_garments_in_inventory() throws Throwable {
+        Assert.assertEquals(true, true);
+    }
 }
