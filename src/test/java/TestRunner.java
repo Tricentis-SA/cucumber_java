ac2010ee7bb05328c03e018f3d59afbf4d28adb9
jarrod-grayson <52757778+jarrod-grayson@users.noreply.github.com>
2021-07-08 11:10:25 -0400

    Update TestRunner.java

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -7,6 +7,6 @@ import cucumber.api.CucumberOptions;
 @CucumberOptions(format={"pretty",
         "html:build/test-results/test-report"},
         features={"src/test/resources/features"})
-@CucumberOptions(plugin = {"pretty", "json:target/cucumber-json-report2.json"})
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber-json-report.json"})
 
 public class TestRunner { }
